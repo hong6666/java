c24a1ffcefd94f82f056e26ea0ec9f05fc710039
hong666 <1345455432@qq.com>
2020-01-13 08:57:44 +0800

    守护线程，线程同步Synchronized问题

--- src/lhh/thread/chapter02/DaemonThread.java
+++ src/lhh/thread/chapter02/DaemonThread.java
@@ -8,25 +8,24 @@ package lhh.thread.chapter02;
  */
 public class DaemonThread {
     public static void main(String[] args) throws InterruptedException{
-
+        //main线程开始
         Thread thread = new Thread(() ->
         {
           while(true)
           {
+              System.out.println("my thread is running");
               try
               {
-                  Thread.sleep(1);
+                  Thread.sleep(500);
               }catch (InterruptedException e)
               {
                   e.printStackTrace();
               }
           }
         });
-
         thread.setDaemon(true); //将thread设置为守护线程
-
-        thread.start();
-        Thread.sleep(2_000L);
+        thread.start();//启动thread线程
+        Thread.sleep(2000);
         System.out.println("Main thread finished lifecycle.");
     }
 }
---
+++ src/lhh/thread/liaoxuefengThread/ThreadSynchronizedApp.java
@@ -0,0 +1,67 @@
+package lhh.thread.liaoxuefengThread;
+
+/**
+ * @program: IdeaJava
+ * @Date: 2020/1/11 11:14
+ * @Author: lhh
+ * @Description: 线程同步问题
+ */
+public class ThreadSynchronizedApp {
+    public static void main(String[] args) throws Exception{
+        /*Thread add = new AddThread();
+        Thread dec = new DecThread();
+        add.start();
+        dec.start();
+        add.join();
+        dec.join();*/
+
+
+        Thread add = new Thread(()->
+        {
+           for(int i = 0;i < 10000;i++)
+           {
+               synchronized(Counter.class)
+               {
+                   Counter.count++;
+               }
+           }
+        });
+        Thread dec = new Thread(()->
+        {
+            for(int i = 0;i < 10000;i++)
+            {
+                synchronized(Counter.class)
+                {
+                    Counter.count--;
+                }
+            }
+        });
+        add.start();
+        dec.start();
+        add.join();
+        dec.join();
+        System.out.println(Counter.count);
+
+    }
+}
+class Counter
+{
+    public static int count = 0;
+}
+
+class AddThread extends Thread
+{
+    public void run()
+    {
+        for (int i = 0;i < 10000;i++)Counter.count++;
+    }
+}
+class DecThread extends Thread
+{
+    public void run()
+    {
+        for(int i = 0;i < 10000;i++)Counter.count--;
+    }
+}
+
+
