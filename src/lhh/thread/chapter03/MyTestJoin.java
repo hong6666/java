99d2d44d13d904c7ef6097aed208ebdc4c695f51
hong666 <1345455432@qq.com>
2020-01-17 10:52:23 +0800

    join方法，yield方法添加描述

---
+++ src/lhh/thread/chapter03/MyTestJoin.java
@@ -0,0 +1,53 @@
+package lhh.thread.chapter03;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @program: IdeaJava
+ * @Date: 2020/1/17 9:27
+ * @Author: lhh
+ * @Description:
+ */
+public class MyTestJoin {
+    public static void main(String[] args) throws Exception{
+        Thread thread1 = MyTestJoin.create(1);
+        Thread thread2 = MyTestJoin.create(2);
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0;i < 4;i++)
+        {
+            System.out.println("main?="+Thread.currentThread().getName() + "#" + i);
+            TimeUnit.SECONDS.sleep(1);
+        }
+
+    }
+    private static Thread create(int index)
+    {
+        Thread thread = new Thread(()->
+        {
+            for (int i = 0; i < 4;i++)
+            {
+                System.out.println("create name = "+Thread.currentThread().getName()+"#"+i);
+
+
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        },String.valueOf(index));//线程命名
+        return thread;
+    }
+}
+
--- src/lhh/thread/chapter03/ThreadYield.java
+++ src/lhh/thread/chapter03/ThreadYield.java
@@ -6,12 +6,14 @@ import java.util.stream.IntStream;
  * @program: IdeaJava
  * @Date: 2019/12/23 14:52
  * @Author: lhh
- * @Description:
+ * @Description: yield的本质是把当前线程重新置入抢CPU时间的”队列”,
+ * 这是让发起线程让步，但是让不让还得看线程心情。和调用gc一样，回不回收得看心情
+ * 不确定性大
  */
 public class ThreadYield {
     public static void main(String[] args)
     {
-        IntStream.range(0,2).mapToObj(ThreadYield::create).forEach(Thread::start);
+        IntStream.range(0,10).mapToObj(ThreadYield::create).forEach(Thread::start);
     }
     private static Thread create(int index)
     {
