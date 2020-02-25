49da06efeefdf8c75c61ce211ed411568dc9a5b8
hong666 <1345455432@qq.com>
2020-02-25 17:13:10 +0800

    adapter适配者模式复习

---
+++ src/lhh/pattern/adapterPattern/adapterPattern/ClassAdapterTest.java
@@ -0,0 +1,44 @@
+package lhh.pattern.adapterPattern.adapterPattern;
+
+/**
+ * @program: IdeaJava
+ * @Date: 2020/2/25 9:52
+ * @Author: lhh
+ * @Description:
+ */
+
+// 目标接口,我们使用的接口
+interface Target {
+    void request();
+}
+
+//适配器接口，不兼容，不能直接使用
+class Adaptee {
+    public void specificRequest()
+    {
+        System.out.println("适配者中的业务代码被调用！");
+    }
+}
+
+//类适配器类，继承适配器
+class ClassAdapter extends Adaptee implements Target {
+
+    public void request()
+    {
+        specificRequest();
+    }
+}
+
+//客户端代码
+public class ClassAdapterTest {
+    public static void main(String[] args) {
+        System.out.println("类适配器模式测试：");
+        Target target = new ClassAdapter();
+        target.request();
+
+    }
+
+
+
+
+}
