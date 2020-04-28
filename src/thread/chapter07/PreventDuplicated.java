d54a67d39937cd278703aa4e08d010b65d81e120
lihonghao <1345455432@qq.com>
2020-04-28 10:52:48 +0800

    fix:Hook线程实战修改，还是有点偏差，线程只能强制kill，没有输出对应信息

--- src/thread/chapter07/PreventDuplicated.java
+++ src/thread/chapter07/PreventDuplicated.java
@@ -4,9 +4,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.PosixFilePermission;
-import java.nio.file.attribute.PosixFilePermissions;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -14,6 +11,10 @@ import java.util.concurrent.TimeUnit;
  * @Date: 2020/4/24 19:36
  * @Author: lhh
  * @Description: Hook线程实战，防止某个程序被重复启动，
+ * 钩子线程hook，只有在收到退出信号的时候会被执行。可以执行一些资源释放的工作
+ * ，如关闭文件句柄、socket连接、数据库connection等。尽量不要在Hook线程中执行一些
+ * 耗时非常长的操作，会导致程序迟迟不能退出。切记如果强制杀死线程，进程将不会收到
+ * 任何中断信号。
  */
 public class PreventDuplicated {
 
@@ -21,13 +22,13 @@ public class PreventDuplicated {
 
     private final static String LOCK_FILE = ".lock";
 
-    private final static String PREMISSIONS = "rw------";
+    private final static String PERMISSIONS = "rw------";
 
     public static void main(String[] args) throws IOException
     {
 
         //注入Hook线程，在程序退出时删除lock文件
-        Runtime.getRuntime().addShutdownHook(new Thread(()->
+        Runtime.getRuntime().addShutdownHook(new Thread(() ->
         {
             System.out.println("The program received kill SIGNAL");
             getLockFile().toFile().delete();
@@ -37,10 +38,10 @@ public class PreventDuplicated {
         checkRunning();
 
         //简单模拟当前程序正在运行
-        for(;;)
+        for(; ; )
         {
             try {
-                TimeUnit.MILLISECONDS.sleep(1);
+                TimeUnit.SECONDS.sleep(1);
                 System.out.println("program is running");
             } catch (InterruptedException e)
             {
@@ -58,9 +59,7 @@ public class PreventDuplicated {
             throw new RuntimeException("The program already running");
         }
 
-        Set<PosixFilePermission> perms = PosixFilePermissions.fromString(PREMISSIONS);
-
-        Files.createFile(path,PosixFilePermissions.asFileAttribute(perms));
+        Files.createFile(path);
 
     }
 
